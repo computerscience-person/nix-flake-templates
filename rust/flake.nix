5c294efc8b039be40289c16874ddc4b0902a14dc
Oliver Ladores <oliver.ladores@wvsu.edu.ph>
2024-08-07 18:00:57 +0800

    Added flake template for rust development.
    - NEW: Added flake template for rust development in ./rust.

---
+++ rust/flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "The base nix flake.";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = { nixpkgs, ... }@inputs: let
+    systems = [ "x86_64-linux" "aarch64-linux" ];
+    eachSystem = nixpkgs.lib.genAttrs systems;
+    withPkgs = system: nixpkgs.legacyPackages.${system};
+    withFenix = system: inputs.fenix.packages.${system};
+  in {
+    devShells = eachSystem (system: let
+      pkgs = withPkgs system;
+      fenix = withFenix system;
+    in { 
+      default = pkgs.mkShell.override { 
+        stdenv = pkgs.mold-wrapped.stdenv;
+      } {
+        packages = [
+          fenix.complete.toolchain
+        ];
+      };
+    });
+  };
+}
