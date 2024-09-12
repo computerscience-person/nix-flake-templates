223088f772baa70a77f2ae295ee799fe9ff37434
Oliver Ladores <oliver.ladores@wvsu.edu.ph>
2024-09-12 19:04:27 +0800

    Added android deps to flutter with android.
    - NEW: Added Android SDK to flutter_with_android template.

--- dart/flutter_with_android/flake.nix
+++ dart/flutter_with_android/flake.nix
@@ -14,10 +14,23 @@
     devShells = eachSystem (system: let
       pkgs = withPkgs system;
       jdk = pkgs.temurin-bin-21;
+      androidtools = pkgs.androidenv.composeAndroidPackages {
+        cmdLineToolsVersion = "13.0";
+        platformTools = "35.0.1";
+        buildTools = "34.0.0";
+        platformsVersions = [ "32" "33" "34" ];
+        emulator = "35.1.4";
+        includeSources = false;
+        incldeSystemImages = false;
+        abiVersions = [ "arm64-v8a" ];
+        useGoogleAPIs = false;
+        useGoogleTVAddOns = false;
+      };
     in { 
       default = pkgs.mkShell {
         packages = [
           jdk
+          androidtools.androidsdk
           pkgs.flutter
           pkgs.gradle
         ];
